37f14d5c656f3471cb2602c88548015040e81939
isaquemarianoazeredo <isaquemarianoazeredo@gmail.com>
2025-10-17 16:19:36 -0300

    glassmorphism makeover

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
-const drawButton = document.querySelector("#draw-button")
+const drawButton = document.querySelector("#draw-button");
 
-drawButton.addEventListener("click", generateNumbers)
+drawButton.addEventListener("click", generateNumbers);
 
 function generateNumbers() {
-    const quantity = parseInt(document.querySelector("#quantity-input").value)
-    const min = Math.ceil(document.querySelector("#min-input").value)
-    const max = Math.floor(document.querySelector("#max-input").value)
+    const quantity = parseInt(document.querySelector("#quantity-input").value);
+    const min = Math.ceil(document.querySelector("#min-input").value);
+    const max = Math.floor(document.querySelector("#max-input").value);
 
-    const results = []
+    const results = [];
 
     for (let i = 0; i < quantity; i++) {
-        const number = Math.floor(Math.random() * (max - min + 1) + min)
-        results.push(number)
+        const number = Math.floor(Math.random() * (max - min + 1) + min);
+        results.push(number);
     }
 
-    alert(results.join(", "))
+    alert(results.join(", "));
 }
